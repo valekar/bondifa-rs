02c2fbe218cc207f635eb9f6e55fe7cb0f8beb5a
valekar <srinivas.valekar@gmail.com>
2021-08-02 22:27:26 +0100

    first api added

--- bonfida-consumer/src/main.rs
+++ bonfida-consumer/src/main.rs
@@ -1,7 +1,17 @@
 use bonfida;
+use bonfida::api::*;
+use bonfida::general::General;
 
 fn main() {
     println!("Hello, world!");
 
     //bonfida::testing();
+
+    let general: General = Bonfida::new();
+    let result = general.get_all_pairs();
+
+    match result {
+        Ok(result) => println!("{:?}", result.data),
+        Err(er) => println!("{:?}", er),
+    }
 }
