c2c7fbd89c32f2b5c43ec1f9646b8592b7f8331d
valekar <srinivas.valekar@gmail.com>
2021-08-06 22:21:44 +0100

    added string_or_struct

--- bonfida-consumer/src/main.rs
+++ bonfida-consumer/src/main.rs
@@ -8,10 +8,42 @@ fn main() {
     //bonfida::testing();
 
     let market: Market = Bonfida::new();
-    // let result = general.get_all_pairs();
+    // let result = market.get_all_pairs();
 
     // match result {
     //     Ok(result) => println!("{:?}", result.data),
     //     Err(er) => println!("{:?}", er),
     // }
+
+    // let result = market.get_all_24hr_trades();
+    // match result {
+    //     Ok(result) => println!("{:?}", result.data),
+    //     Err(er) => println!("{:?}", er),
+    // }
+
+    // let result = market.get_trades_for_market(String::from("FIDARAY"));
+    // match result {
+    //     Ok(result) => println!("{:?}", result.data),
+    //     Err(er) => println!("{:?}", er),
+    // }
+
+    // let result = market.get_order_books_for_market(String::from("FIDARAY"));
+    // match result {
+    //     Ok(result) => println!("{:?}", result.data),
+    //     Err(er) => println!("{:?}", er),
+    // }
+
+    // let result = market.get_volumes_for_market(String::from("FIDARAY"));
+    // match result {
+    //     Ok(result) => println!("{:?}", result.data),
+    //     Err(er) => println!("{:?}", er),
+    // }
+
+    let result = market.get_trades_for_market_address(String::from(
+        "9wH4Krv8Vim3op3JAu5NGZQdGxU8HLGAHZh3K77CemxC",
+    ));
+    match result {
+        Ok(result) => println!("{:?}", result.data),
+        Err(er) => println!("{:?}", er),
+    }
 }
